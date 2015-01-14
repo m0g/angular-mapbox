6d2c2232af53cdb4f022bfaa129e7b8140a9b161
Loïc Nogues <nogues.loic@gmail.com>
2015-01-14 15:11:05 +0300

    added a legend

--- dist/angular-mapbox.js
+++ dist/angular-mapbox.js
@@ -204,6 +204,34 @@ var featureListener = function(featureLayer, map, scope, $mdToast) {
   });
 };
 
+angular.module('angularMapbox').directive('legend', function() {
+  return {
+    restrict: 'E',
+    require: '^mapbox',
+    scope: true,
+    link: function(scope, element, attrs, controller) {
+      controller.getMap().then(function(map) {
+        map.legendControl.addLegend(getLegendHTML());
+      });
+    }
+  }
+});
+
+var getLegendHTML = function() {
+  var responses = [{ color: '#f44336', content: 'yes' },
+                   { color: '#3f51b5', content: 'no' }],
+  labels = [];
+
+  responses.forEach(function(response) {
+    labels.push(
+      '<li><span class="swatch" style="background:' + response.color + '"></span> ' +
+      response.content + '</li>'
+    );
+  });
+
+  return '<span>Legend</span><ul>' + labels.join('') + '</ul>';
+}
+
 angular.module('angularMapbox').directive('mapbox', function($compile, $q) {
   var _mapboxMap;
 
