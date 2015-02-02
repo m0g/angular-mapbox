387cb01783da5b70fd8708872172ce6a0682992c
Loïc Nogues <nogues.loic@gmail.com>
2015-02-02 16:14:26 +0300

    added zoom controls on the wards level

--- dist/angular-mapbox.js
+++ dist/angular-mapbox.js
@@ -6,21 +6,49 @@ angular.module('angularMapbox').directive('backButton', function() {
     restrict: 'E',
     require: '^mapbox',
     scope: true,
-    template: '<a class="back md-fab md-primary md-default-theme md-cyan-theme md-button" ng-click="back()" ng-show="show"><i class="fa fa-times"></i></a>',
+    template: '<a class="back md-fab md-primary md-default-theme md-cyan-theme md-button"' +
+                 'ng-click="back()" ng-show="show"><i class="fa fa-times"></i></a>' +
+              '<a class="back md-fab md-primary md-default-theme md-cyan-theme md-button"' +
+                 'ng-click="zoomIn()" ng-show="show"><i class="fa fa-plus"></i></a>' +
+              '<a class="back md-fab md-primary md-default-theme md-cyan-theme md-button"' +
+                 'ng-click="zoomOut()" ng-show="show"><i class="fa fa-minus"></i></a>',
+
     controller: function($scope) {
       var parent = $scope.$parent.$parent;
 
       $scope.show = false;
+      $scope.zoomMin = 0;
+
+      parent.getMap().then(function(map) {
+        $scope.zoomMin = Math.round(map.getZoom());
+        console.log('zoom min init', $scope.zoomMin);
+      });
 
       parent.$watch('showBack', function() {
         $scope.show = parent.showBack;
       });
+
+      $scope.zoomIn = function() {
+        parent.getMap().then(function(map) {
+          var z = Math.round(map.getZoom());
+          map.setZoom(z + 1);
+          map.dragging.enable();
+        });
+      };
+
+      $scope.zoomOut = function() {
+        parent.getMap().then(function(map) {
+          var z = Math.round(map.getZoom());
+          if (z - 1 > 6) map.setZoom(z - 1);
+        });
+      };
 
       $scope.back = function() {
         parent.showBack = false;
 
         parent.getMap().then(function(map) {
           var region = parent.featureLayers[0];
+          map.dragging.disable();
 
           parent.featureLayers.forEach(function(layer, index) {
             if (index > 0) layer.clearLayers();
@@ -28,9 +56,7 @@ angular.module('angularMapbox').directive('backButton', function() {
 
           console.log(parent);
           if (parent.$parent.hasOwnProperty('region'))
-            //parent.$parent.$apply(function() {
-              parent.$parent.region = null;
-            //});
+            parent.$parent.region = null;
 
           map.fitBounds(region.getBounds());
         });
